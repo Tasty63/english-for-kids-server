cfb834646a5e2086365d91b69f80a818cf14d0ff
Tasty <sapegin.petya@yandex.ru>
2021-07-17 00:56:38 +0400

    feat: add cloudinary

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,10 @@ import path from 'path';
 export const PORT = process.env.PORT || 5000;
 export const mongoURL =
   'mongodb+srv://Tasty:tasty@cluster0.tlprk.mongodb.net/english-for-kids?retryWrites=true&w=majority';
+export const cloudinaryURL = 'cloudinary://739318174681858:lfHGHpNw5v_kJTo_fzfL8gjuF68@tasty63';
+export const cloudinaryAPIKey = 739318174681858;
+export const clodinaryAPISecret = 'lfHGHpNw5v_kJTo_fzfL8gjuF68';
 export const publicPath = path.resolve(__dirname, '../public');
-
 export const jwtSecretKey = 'frontend';
 
 export enum StatusCodes {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
+require('dotenv').config();
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import { PORT, mongoURL, publicPath } from './config';
-
+import { PORT, mongoURL, publicPath, cloudinaryURL } from './config';
+import cloudinary from 'cloudinary';
+import multer from 'multer';
 import authorizationRouter from './authorization/authorization-router';
 import categoryRoter from './category/category-router';
 import statisticsRouter from './statistics/statistics-router';
 
+// cloudinary.v2.uploader.upload('public/images/angry.jpg', { folder: 'images', public_id: 'angry' }, (error, result) => {
+//   console.log(result, error);
+// });
+
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(/^(?!\/api\/)/, express.static(publicPath));
 
