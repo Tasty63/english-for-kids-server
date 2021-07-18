963f95a10950371609dcab004afa9cbbbf5988e1
Tasty <sapegin.petya@yandex.ru>
2021-07-19 00:08:09 +0400

    feat: make update category api

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -77,4 +77,43 @@ categoryRouter.delete(
   }
 );
 
+categoryRouter.put('/:id', imageLoader.single('image'), async (request: express.Request, result: express.Response) => {
+  try {
+    const { categoryName } = request.body;
+    const path = request.file?.path;
+    const cloudinary_id = request.file?.filename;
+
+    const oldCategory = await CategoryModel.findOne({ _id: request.params.id });
+
+    if (!oldCategory) {
+      return result.status(StatusCodes.BadRequest).json({ message: 'Category does not exist' });
+    }
+
+    const category = await CategoryModel.updateOne(
+      { _id: request.params.id },
+      {
+        name: categoryName || oldCategory?.name,
+        preview: path || oldCategory?.preview,
+        cloudinary_id: cloudinary_id || oldCategory?.cloudinary_id,
+      }
+    );
+
+    if (cloudinary_id && oldCategory) {
+      cloudinary.v2.uploader.destroy(oldCategory.cloudinary_id);
+    }
+
+    const categories = await CategoryModel.find();
+
+    if (!categories) {
+      return result.json({});
+    }
+
+    return result.json(categories);
+  } catch (error) {
+    return result
+      .status(StatusCodes.InternalServerError)
+      .json({ message: 'Something went wrong. Please try again later' });
+  }
+});
+
 export default categoryRouter;
