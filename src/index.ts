d8dfa025fbd8c8ad6dfbfd5107ffe6a2cd735d1e
Tasty <sapegin.petya@yandex.ru>
2021-07-19 15:32:08 +0400

    refactor: update cors (again :( )

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,14 @@ import statisticsRouter from './statistics/statistics-router';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    preflightContinue: false,
+  })
+);
 
 app.use('/api/auth', authorizationRouter);
 app.use('/api/category', categoryRoter);
