c2edf732179b09b5fad6d7181f46e30cf4b2b396
Tasty <sapegin.petya@yandex.ru>
2021-07-19 15:38:15 +0400

    docs: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,7 @@ import statisticsRouter from './statistics/statistics-router';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(
-  cors({
-    allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: '*',
-    preflightContinue: false,
-  })
-);
+app.use(cors());
 
 app.use('/api/auth', authorizationRouter);
 app.use('/api/category', categoryRoter);
