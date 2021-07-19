ebdf627fdab1970cc04366505e30ef9791803064
Tasty <sapegin.petya@yandex.ru>
2021-07-19 14:53:35 +0400

    refactor: update cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import statisticsRouter from './statistics/statistics-router';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors({ origin: true }));
 
 app.use('/api/auth', authorizationRouter);
 app.use('/api/category', categoryRoter);
