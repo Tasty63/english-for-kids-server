6bfce1e71a4fc86550efab53e7f10edd1828c0af
Tasty <sapegin.petya@yandex.ru>
2021-07-19 15:44:09 +0400

    refactor: update cors

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/api/auth', authorizationRouter);
 app.use('/api/category', categoryRoter);
 app.use('/api/statistics', statisticsRouter);
